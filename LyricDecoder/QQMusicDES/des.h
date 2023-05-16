b14047472c28392789bcd505af6cf7cf657eb695
poly000 <1348292515@qq.com>
2023-05-16 11:22:03 +0800

    use system zlib headers

--- LyricDecoder/QQMusicDES/des.h
+++ LyricDecoder/QQMusicDES/des.h
@@ -16,16 +16,13 @@
 #include <stddef.h>
 
 /****************************** MACROS ******************************/
-#define DES_BLOCK_SIZE 8                // DES operates on 8 bytes at a time
+#define DES_BLOCK_SIZE 8  // DES operates on 8 bytes at a time
 
 /**************************** DATA TYPES ****************************/
-typedef unsigned char BYTE;             // 8-bit byte
-typedef unsigned int  WORD;             // 32-bit word, change to "long" for 16-bit machines
+typedef unsigned char BYTE;  // 8-bit byte
+typedef unsigned int WORD;  // 32-bit word, change to "long" for 16-bit machines
 
-typedef enum {
-	DES_ENCRYPT,
-	DES_DECRYPT
-} DES_MODE;
+typedef enum { DES_ENCRYPT, DES_DECRYPT } DES_MODE;
 
 /*********************** FUNCTION DECLARATIONS **********************/
 #ifdef __cplusplus
@@ -34,10 +31,11 @@ extern "C" {
 void des_key_setup(const BYTE key[], BYTE schedule[][6], DES_MODE mode);
 void des_crypt(const BYTE in[], BYTE out[], const BYTE key[][6]);
 
-void three_des_key_setup(const BYTE key[], BYTE schedule[][16][6], DES_MODE mode);
+void three_des_key_setup(const BYTE key[], BYTE schedule[][16][6],
+                         DES_MODE mode);
 void three_des_crypt(const BYTE in[], BYTE out[], const BYTE key[][16][6]);
 #ifdef __cplusplus
 }
 #endif
 
-#endif   // DES_H
+#endif  // DES_H
--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-## LyricDecoder
+# LyricDecoder
 
-slightly modified version of https://github.com/SuJiKiNen/LyricDecoder for use with rust.
+## dep
 
-build the project with simply 
-```bash
-emcc LyricDecoder.c lrcdec.c QQMusicDES/des.c -s USE_ZLIB=1 -o lrcdec.cjs
-```
+`zlib`
 
-### Licence
+## Licence
 
 https://github.com/SuJiKiNen/LyricDecoder/blob/master/LICENSE
